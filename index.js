4dbc437ca3582c2c736dc71633fc203e142021fc
Jaime Zuspann <jaimeadele@gmail.com>
2017-10-25 01:35:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  n = parseInt(n);
+  let rankedList = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < rankedList.length; i++){
+    rankedList[i].innerHTML = (i + n).toString();
+  }
+}
+
+function deepestChild(){
+  let list = document.querySelectorAll('div#grand-node div');
+  let current;
+  for (let i = 0; i < list.length; i++){
+    current = list[i];
+  }
+  return current;
+
+  /*let list = document.querySelectorAll('div');
+  let array = Array.from(list);
+  let current;
+  for (let i = 0; i < array.length; i++){
+    //console.log(list[i].innerHTML);
+    current = array[i];
+    //console.log(current);
+    //current = list[i];
+  }
+  console.log(array);
+  //console.log(current);*/
+}
+
+deepestChild();
